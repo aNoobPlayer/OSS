af750401ca17f8bd450a412dd4771eefbc27b26a
aNoobPlayer <120468380+aNoobPlayer@users.noreply.github.com>
2024-12-21 16:22:50 +0700

    Update Facilities.jsx

--- app/src/pages/Facilities.jsx
+++ app/src/pages/Facilities.jsx
@@ -15,7 +15,7 @@ export default function Facilities() {
         aria-label="Facilities Introduction"
       >
         <p>
-          We want your stay at our lush hotel to be truly unforgettable. That is
+         1111111111111111 We want your stay at our lush hotel to be truly unforgettable. That is
           why we give special attention to all of your needs so that we can ensure
           an experience quite uniquw. Luxury hotels offers the perfect setting
           with stunning views for leisure and our modern luxury resort facilities
